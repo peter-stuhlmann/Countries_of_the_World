ab3372469a2f73ae5f8ace41bbc9a25f2862b76b
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-05 10:57:56 +0100

    Put everything in one function and added an eventListener (button click)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
-fetch("https://restcountries.eu/rest/v2/callingcode/49")
-    .then(
-        response => response.json()
-    )
-    .then(
-        country => {
-            let countries = [];
+function countries() {
 
-            countries.push(`
+    let inputCountry = document.querySelector('#country-input').value || '49'
+
+    fetch(`https://restcountries.eu/rest/v2/callingcode/${inputCountry}`)
+        .then(
+            response => response.json()
+        )
+        .then(
+            country => {
+                let countries = [];
+
+                countries.push(`
                 <h2>${country[0].name}</h2>
                 <div>${country[0].name} <i>(in ${country[0].languages[0].name}: "${country[0].nativeName}")</i> is a country in ${country[0].subregion}. The capital is ${country[0].capital}. ${country[0].name} has about ${country[0].population} inhabitants.</div> 
             `)
 
-            document.querySelector('#country').innerHTML = countries.join('')
-        }
-    )
-    .catch(
-        err => console.log(`Error: ${err}`)
-    )
+                document.querySelector('#country').innerHTML = countries.join('')
+            }
+        )
+        .catch(
+            err => console.log(`Error: ${err}`)
+        )
+}
+
+countries()
+document.querySelector('#country-input-button').addEventListener('click', countries)
