3c53d985a7349124a3b55037976e607b3476b53c
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-05 11:24:54 +0100

    Added api code for the flag svg

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,8 @@ function countries() {
 
                 countries.push(`
                 <h2>${country[0].name} (${country[0].alpha2Code})</h2>
-                <div>${country[0].name} <i>(in ${country[0].languages[0].name}: "${country[0].nativeName}")</i> is a country in ${country[0].subregion}. The capital is ${country[0].capital}. ${country[0].name} has about ${country[0].population} inhabitants.</div> 
+                <div>${country[0].name} <i>(in ${country[0].languages[0].name}: "${country[0].nativeName}")</i> is a country in ${country[0].subregion}. The capital is ${country[0].capital}. ${country[0].name} has about ${country[0].population} inhabitants.</div>
+                <div class="flag"><img src="${country[0].flag}"></div> 
             `)
 
                 document.querySelector('#country').innerHTML = countries.join('')
