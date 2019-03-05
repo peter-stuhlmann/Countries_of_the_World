0819dbc319d4f05dbb6d187867650f18653e3d48
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-03-05 11:09:49 +0100

    Added eventListener (press enter instead of button click)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function countries() {
                 let countries = [];
 
                 countries.push(`
-                <h2>${country[0].name}</h2>
+                <h2>${country[0].name} (${country[0].alpha2Code})</h2>
                 <div>${country[0].name} <i>(in ${country[0].languages[0].name}: "${country[0].nativeName}")</i> is a country in ${country[0].subregion}. The capital is ${country[0].capital}. ${country[0].name} has about ${country[0].population} inhabitants.</div> 
             `)
 
@@ -25,3 +25,12 @@ function countries() {
 
 countries()
 document.querySelector('#country-input-button').addEventListener('click', countries)
+
+
+// 'Enter' instead of button click
+
+document.querySelector('#country-input').addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        countries()
+    }
+});
